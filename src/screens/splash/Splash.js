d6aefb3e60f863071ecd21f5a83d5f2da5b0aaad
shameemkpofficial <shameemkpfficial@gmail.com>
2022-09-09 21:02:06 +0530

    state changing options

--- src/screens/splash/Splash.js
+++ src/screens/splash/Splash.js
@@ -1,25 +1,30 @@
-import React, {useEffect ,useContext} from 'react';
+import React, {useEffect, useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {useTheme} from '@theme/ThemeContext';
+import Lotties from 'lottie-react-native';
 import Fonts from '../../theme/Fonts';
-import { AppContext } from '../../common/Constants'
+import {AppContext} from '../../common/Constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Toast from 'react-native-simple-toast';
 
 export default function Splash({navigation}) {
   const {colors, isDark} = useTheme();
-  const { user, setUser, baseurl } = useContext(AppContext);
+  // const {user, setUser, baseurl} = useContext(AppContext);
 
-useEffect(() => {
-  setTimeout(() => {
-    navigation.navigate('Home') 
-  }, 1000);
-}, [])
-
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 3000);
+  }, []);
 
   return (
-    <View style={[styles.mainContainer, {backgroundColor: colors.primary}]}>
-      <Text style={{fontFamily : Fonts.regular , color:"#FFF"}}>Hello , React Native Devs</Text>
+    <View style={[styles.mainContainer]}>
+      <Lotties
+        source={require('../../assets/animation/Healthy.json')}
+        autoPlay
+        loop={false}
+        style={{height: 200}}></Lotties>
+      <Text style={styles.textStyle}>Covid-19 India</Text>
     </View>
   );
 }
@@ -29,5 +34,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  textStyle: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: 'black',
   },
 });
